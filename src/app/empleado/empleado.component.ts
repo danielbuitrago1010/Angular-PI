905ba4fcaeea603b4e736b7b7e5d85179f16bbc9
danielbm10 <danielbuitrago11@gmail.com>
2021-08-19 17:40:48 -0500

    initial commit

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  //template: "<p> Hola</p>",
+  styleUrls: ['./empleado.component.css']
+  //styles:["p{background-color:red;}"]
+
+})
+
+
+export class EmpleadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
